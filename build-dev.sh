ab11ea2efd0978513909c83af6e4408c45e2677b
jdmartin <jdmgit@preludes.org>
2023-08-28 20:19:46 -0700

    Build, deps changes

--- build-dev.sh
+++ build-dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bundle exec jekyll serve --config _config.yml,_config_dev.yml --watch --drafts -o;
+bundle exec jekyll serve -H 0.0.0.0 --port 8083 --config _config.yml,_config_dev.yml --watch --drafts --destination _dev_build;
