4f72c3a7d850ce8ddaf3dda43d934b677a6a5bff
Jon Martin <jdmgit@preludes.org>
2024-05-30 18:32:35 -0400

    Use .env with build_dev to handle host

--- build-dev.sh
+++ build-dev.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-bundle exec jekyll serve -H 0.0.0.0 --port 8083 --config _config.yml,_config_dev.yml --watch --drafts --destination _dev_build;
+# Load .env file
+if [ -f .env ]; then
+    export $(cat .env | sed 's/#.*//g' | xargs)
+else
+    echo ".env file not found!"
+    exit 1
+fi
+
+bundle exec jekyll serve -H $HOST --port 8083 --config _config.yml,_config_dev.yml --watch --drafts --destination _dev_build;
